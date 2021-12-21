233b9a4a2607ae398670de92783112ca5ef3eaf4
rodolfodevpleno <rodolfosilvaa16@outlook.com>
2021-12-20 23:55:12 -0300

    feature: init estudo JAMStack utilizando o Prismic CMS - criando página de posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+
+    return(
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & yarn Workspace</strong>
+                        <p>In this guide, you will learn how to create a Monorepo to manager multiple package with a shared buy</p>
+                    </a>
+
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & yarn Workspace</strong>
+                        <p>In this guide, you will learn how to create a Monorepo to manager multiple package with a shared buy</p>
+                    </a>
+
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & yarn Workspace</strong>
+                        <p>In this guide, you will learn how to create a Monorepo to manager multiple package with a shared buy</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    )
+
+}
